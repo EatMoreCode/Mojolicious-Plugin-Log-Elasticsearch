0ec8ac81c73a5a57db270b0b846d87146b4ecd1c
Justin Hawkins <justin@hawkins.id.au>
2015-07-20 18:39:23 +0930

    Add configuration and Elastic index setup.

--- lib/Mojolicious/Plugin/Log/Elasticsearch.pm
+++ lib/Mojolicious/Plugin/Log/Elasticsearch.pm
@@ -1,4 +1,5 @@
 package Mojolicious::Plugin::Log::Elasticsearch;
+
 use Mojo::Base 'Mojolicious::Plugin';
 
 use Time::HiRes qw/time/;
@@ -7,7 +8,24 @@ use Mojo::JSON  qw/encode_json/;
 our $VERSION = '0.01';
 
 sub register {
-  my ($self, $app) = @_;
+  my ($self, $app, $conf) = @_;
+
+  my $index = $conf->{index} || die "no elasticsearch index provided";
+  my $type  = $conf->{type}  || die "no elasticsearch type provided";
+  my $es_url = $conf->{elasticsearch_url} || die "no elasticsearch url provided";
+
+  my $index_meta = {
+    $type => {
+      "_timestamp" => { enabled => 1, store => 1 },
+      "properties" => { 
+        'ip' => { 'type' => 'ip', 'store' => 1 },
+      }
+    }
+  };
+
+  my $url = "${es_url}/${index}/${type}/_mapping";
+  my $tx = $app->ua->post($url, json => $index_meta);
+
   $app->hook(before_dispatch => sub {
     my $c = shift;
     $c->stash->{'mojolicious-plugin-log-elasticsearch.start'} = time();
@@ -15,21 +33,33 @@ sub register {
 
   $app->hook(after_dispatch => sub {
     my $c = shift;
+    my @n = gmtime();
+    my $t = sprintf("%04d-%02d-%02dT%02d:%02d:%02dZ", $n[5]+1900, $n[4]+1, $n[3],
+                                                      @n[2,1,0]);
     my $dur = time() - $c->stash->{'mojolicious-plugin-log-elasticsearch.start'};
     
-    my $json = encode_json { ip     => $c->tx->remote_address, 
-                             path   => $c->req->url->to_abs->path, 
-                             code   => $c->res->code,
-                             method => $c->req->method,
-                             time   => $dur };
+    my $data = { ip     => $c->tx->remote_address, 
+                 path   => $c->req->url->to_abs->path, 
+                 code   => $c->res->code,
+                 method => $c->req->method,
+                 time   => $dur,
+                 # timestamp => $t,
+                 # _timestamp => $t,
+    };
 
-    # $c->app->ua->put('http://localhost:9200/mojo/log', form stuff here $ => sub {
-      # my ($ua, $tx) = @_;
-    # });
+    my $url = "${es_url}/${index}/${type}/?timestamp=${t}";
+    $c->app->ua->post($url, json => $data, sub {
+      my ($ua, $tx) = @_;
+      if (! $tx) {
+        $c->app->error("could not log to elasticsearch");
+      }
+      elsif ($tx->res->code !~ /^20./) {
+        $c->app->error("could not log to elasticsearch - " . $tx->res->body);
+      }
+    });
   });
 }
 
-  
 1;
 __END__
 
